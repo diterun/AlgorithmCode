0f3a7d3e0434f3c870970432281a2ec3e0d606c0
Ha Nam <moons3000@naver.com>
2019-01-23 10:36:25 +0900

    5397 추가

---
+++ baek/BJ_5397.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 1000002
+
+char password[MAX];
+int t, i, cur, end, len;
+char input[MAX];
+
+int main(void){
+    cin >> t;
+
+    while(t--){
+        cur = end = 0;
+
+        cin >> input;
+
+        for(i = 0; i < MAX; i++){
+            if (input[i] ==){
+                i++
+            }
+        }
+    }
+}
