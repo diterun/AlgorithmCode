1538eeb2d50e293625605f18370bbc365b392e2f
diterun <diterun@gmail.com>
2019-04-03 21:51:38 +0900

    4월 3일 저녁 집에서

---
+++ java/src/com/d8/Solution_2455_Stains.java
@@ -0,0 +1,29 @@
+package com.d8;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Solution_2455_Stains {
+	static int T, t, n, m, c, result;
+	static int[][] matt;
+
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/2455.txt"));
+		Scanner sc = new Scanner(System.in);
+		
+		T = sc.nextInt();
+		for (t = 1; t <= T; ++t) {
+			m = sc.nextInt();
+			n = sc.nextInt();
+			c = sc.nextInt();
+			
+			matt = new int[m][n];
+			for (int i = 0; i < c; i++) {
+				matt[sc.nextInt() - 1][sc.nextInt() - 1] = 1;
+			}
+			
+		}
+		
+		sc.close();
+	}
+}
