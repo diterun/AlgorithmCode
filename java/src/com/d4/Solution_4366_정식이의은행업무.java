89728f615c1b61a594f63b29b9b8f2c4ef80b507
황하남 <moons3000@naver.com>
2019-04-04 18:03:32 +0900

    4월 4일

---
+++ java/src/com/d4/Solution_4366_정식이의은행업무.java
@@ -0,0 +1,78 @@
+package com.d4;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Solution_4366_정식이의은행업무 {
+	static int T, t, size;
+	static long result;
+	static long[] binary;
+	static char[] line;
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/4366.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		T = Integer.parseInt(br.readLine().trim());
+
+		for (t = 1; t <= T; t++) {
+			line = br.readLine().toCharArray();
+			size = line.length;
+			binary = new long[size];
+			
+			/**Make binary numbers at that can*/
+			for (int i = 0; i < size; i++) {
+				int temp = 1;
+				int num = 0;
+				
+				for (int j = size - 1; j >= 0; j--) {
+					num = line[j] - '0';
+					/**If same j at i, change the bit*/
+					if(i == j) {
+						num = (num + 1) % 2;
+					}
+					binary[i] += num * temp;
+					temp *= 2;
+				}
+			}
+			
+//			for (int i = 0; i < size; i++) {
+//				System.out.println(binary[i]);
+//			}
+			
+			line = br.readLine().toCharArray();
+			size = line.length;
+			for (int i = 0; i < size; i++) {
+				int temp = 1;
+				int num1 = 0;
+				int num2 = 0;
+				long sub1 = 0;
+				long sub2 = 0;
+				
+				for (int j = size - 1; j >= 0; j--) {
+					num1 = num2 = line[j] - '0';
+					if(i == j) {
+						num1 = (num1 + 1) % 3;
+						num2 = (num2 + 2) % 3;
+					}
+					sub1 += num1 * temp;
+					sub2 += num2 * temp;
+					temp *= 3;
+				}
+				
+				for (int j = 0; j < binary.length; j++) {
+					if(sub1 == binary[j] || sub2 == binary[j]) {
+						result = binary[j];
+						i = size;
+						break;
+					}
+				}
+			}
+			
+			System.out.println("#" + t + " " + result);
+		}
+
+		br.close();
+	}
+}
---
+++ java/src/com/none/Solution_4014_end_활주로건설.java
@@ -0,0 +1,161 @@
+package com.none;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_4014_end_활주로건설 {
+	static int T, t;
+	static int n, x, result, prev, now;
+	static int[][] map;
+	static int[] jihyoungCnt = new int[7];
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/4014.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		T = Integer.parseInt(br.readLine().trim());
+
+		for (t = 1; t <= T; t++) {
+			result = 0;
+			
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			x = Integer.parseInt(st.nextToken());
+			map = new int[n][n];
+			
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				boolean can = true;
+				boolean isUp = true;
+				prev = 0;
+				
+				for (int j = 0; j < n; j++) {
+					now = Integer.parseInt(st.nextToken());
+					map[i][j] = now;
+					
+					if(now != prev && prev != 0) {
+						if(now == prev + 1) {
+							if(isUp) {
+								if(jihyoungCnt[prev] >= x) {
+										jihyoungCnt[prev] = 0;
+										jihyoungCnt[now] = 1;
+								} else {
+									jihyoungCnt[prev] = 0;
+									can = false;
+								}
+							} else {
+								if(jihyoungCnt[prev] >= (x * 2)) {
+									jihyoungCnt[prev] = 0;
+									jihyoungCnt[now] = 1;
+								} else {
+									jihyoungCnt[prev] = 0;
+									can = false;
+								}
+							}
+							isUp = true;
+						} else if(now == prev - 1) {
+							if(!isUp) {
+								if(jihyoungCnt[prev] >= x) {
+									jihyoungCnt[prev] = 0;
+									jihyoungCnt[now] = 1;
+								} else {
+									jihyoungCnt[prev] = 0;
+									can = false;
+								}
+							} else {
+								jihyoungCnt[prev] = 0;
+								jihyoungCnt[now] = 1;
+							}
+							isUp = false;
+						} else {
+							jihyoungCnt[prev] = 0;
+							can = false;
+						}
+					} else {
+						jihyoungCnt[now]++;
+					}
+					prev = now;
+				}
+				if(!isUp) {
+					if(jihyoungCnt[now] < x) {
+						can = false;
+					}
+				}
+				jihyoungCnt[now] = 0;
+				if(can) {
+					result++;
+				}
+			}
+			
+			for (int j = 0; j < n; j++) {
+				boolean can = true;
+				boolean isUp = true;
+				prev = 0;
+				
+				for (int i = 0; i < n; i++) {
+					now = map[i][j];
+					
+					if(now != prev && prev != 0) {
+						if(now == prev + 1) {
+							if(isUp) {
+								if(jihyoungCnt[prev] >= x) {
+										jihyoungCnt[prev] = 0;
+										jihyoungCnt[now] = 1;
+								} else {
+									jihyoungCnt[prev] = 0;
+									can = false;
+								}
+							} else {
+								if(jihyoungCnt[prev] >= (x * 2)) {
+									jihyoungCnt[prev] = 0;
+									jihyoungCnt[now] = 1;
+								} else {
+									jihyoungCnt[prev] = 0;
+									can = false;
+								}
+							}
+							isUp = true;
+						} else if(now == prev - 1) {
+							if(!isUp) {
+								if(jihyoungCnt[prev] >= x) {
+									jihyoungCnt[prev] = 0;
+									jihyoungCnt[now] = 1;
+								} else {
+									jihyoungCnt[prev] = 0;
+									can = false;
+								}
+							} else {
+								jihyoungCnt[prev] = 0;
+								jihyoungCnt[now] = 1;
+							}
+							isUp = false;
+						} else {
+							jihyoungCnt[prev] = 0;
+							can = false;
+						}
+					} else {
+						jihyoungCnt[now]++;
+					}
+					prev = now;
+				}
+				
+				if(!isUp) {
+					if(jihyoungCnt[now] < x) {
+						can = false;
+					}
+				}
+				jihyoungCnt[now] = 0;
+				if(can) {
+					result++;
+				}
+			}
+			
+			System.out.println("#" + t + " " + result);
+		}
+
+		br.close();
+	}
+}
