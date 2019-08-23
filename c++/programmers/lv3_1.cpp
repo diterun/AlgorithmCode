39feec5b9728eee60996737621ba91022caa2c54
diterun <diterun@gmail.com>
2019-08-23 18:57:32 +0900

    20190823 1857 dhglfl

--- c++/programmers/lv3_1.cpp
+++ c++/programmers/lv3_1.cpp
@@ -1,106 +1,48 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-vector<int> solution(vector<string> genres, vector<int> plays);
+vector<int> solution(int n, int s);
 
 int main(){
-  vector<string> gen = {"classic", "prp", "classic", "classic", "pop"};
-  vector<int> num = {500, 600, 150, 800, 2500};
-
-  vector<int> result = solution(gen, num);
-
+  vector<int> result = solution(2, 9);
   for(int i = 0; i < result.size(); i++){
-    cout << result[i] << endl;
+    cout << result[i] << " ";
   }
+  cout << endl;
+
+  result = solution(3, 10);
+  for(int i = 0; i < result.size(); i++){
+    cout << result[i] << " ";
+  }
+  cout << endl;
 
   return 0;
 }
 
-bool cmp(const pair<int, int> &a, const pair<int, int> &b){
-    return a.second > b.second;
-}
+vector<int> solution(int n, int s) {
+    vector<int> answer;
 
-vector<int> solution(vector<string> genres, vector<int> plays) {
-  vector<int> answer;
-  string genre[100];
-  int genreCnt = 0;
-  vector<pair<int, int>> track[100];
-  int trackPlays[100][2];
-
-  for(int i = 0; i < 100; i++){
-    genre[i] = "";
-  }
-
-  int inputSize = genres.size();
-
-  for(int i = 0; i < inputSize; i++){
-    cout << genres[i] << " " << plays[i] << endl;
-    bool isExist = false;
-    for(int j = 0; j < genreCnt; j++){
-      if(genre[j] == genres[i]){
-        trackPlays[j][1] += plays[i];
-        track[j].push_back(make_pair(i, plays[i]));
-        genreCnt++;
-
-        isExist = true;
+    if(n > s){
+      answer.push_back(-1);
+      return answer;
+    } else if(s % n == 0){
+      int num = s / n;
+      for(int i = 0; i < n; i++){
+        answer.push_back(num);
       }
+      return answer;
     }
 
-    if(!isExist){
-      genre[genreCnt] = genres[i];
-      trackPlays[genreCnt][0] = genreCnt;
-      trackPlays[genreCnt][1] = plays[i];
-      track[genreCnt].push_back(make_pair(i, plays[i]));
-      genreCnt++;
+    int minNumber = s / n;
+    int otherCnt = s % n;
+
+    for(int i = 0; i < n - otherCnt; i++){
+        answer.push_back(minNumber);
     }
-  }
-
-  for(int i = 0; i < genreCnt - 1; i++){
-    for(int j = 0; j < genreCnt; j++){
-      if(trackPlays[i][1] < trackPlays[j][1]){
-        int temp = trackPlays[i][1];
-        trackPlays[i][1] = trackPlays[j][1];
-        trackPlays[j][1] = temp;
-
-        temp = trackPlays[i][0];
-        trackPlays[i][0] = trackPlays[j][0];
-        trackPlays[j][0] = temp;
-      }
+    for(int i = n - otherCnt; i < n; i++){
+        answer.push_back(minNumber + 1);
     }
-  }
 
-  // for(int i = 0; i < genreCnt; i++){
-  //   sort(track[i].begin(), track[i].end(), cmp);
-  // }
-
-  for(int i = 0; i < genreCnt; i++){
-    int genreSize = track[i].size();
-    int firstCnt = 0, secondCnt = 0;
-    int firstIndex = 0, secondIndex = 0;
-
-    for(int j = 0; j < genreSize; j++){
-      if(secondCnt < track[i][j].second){
-        if(firstCnt < track[i][j].second){
-          secondCnt = firstCnt;
-          secondIndex = firstIndex;
-          firstCnt = track[i][j].second;
-          firstIndex = j;
-        } else if(firstCnt == track[i][j].second){
-          track[i][firstIndex].first
-        } else {
-          secondCnt = track[i][j].second;
-          secondIndex = j;
-        }
-      } else if(secondCnt == track[i][j].second){
-
-      }
-    }
-    for(int j = 0; j < 2 && j < track[i].size(); j++){
-      answer.push_back(track[trackPlays[i][0]][j].first);
-    }
-  }
-
-  return answer;
+    return answer;
 }
--- c++/programmers/lv3_2.cpp
+++ c++/programmers/lv3_2.cpp
@@ -1,17 +1,86 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
-int solution(int n);
+int solution(vector<vector<int>> jobs);
 
 int main(){
-  cout << solution(15) << endl;
+  vector<vector<int>> a = {{0, 3},{1, 9},{2,6}};
+  cout<< a[0][0] << " " << a[0][1] << endl;
+  cout<< a[1][0] << " " << a[1][1] << endl;
+  cout<< a[2][0] << " " << a[2][1] << endl;
+  cout << solution(a) << endl;
 
   return 0;
 }
 
-int solution(int n) {
-  int answer = 0;
+int solution(vector<vector<int>> jobs) {
+  int answer = 0, diskTime = 0;
+  int jobLen = jobs.size();
+  int jobNum = 0, qStart = 0, qEnd = 0;
+  int jobQueue[500][2];
+  bool isEnd = false;
 
-  return answer;
+  for(int i = 0; i < jobLen - 1; i++){
+    for(int j = i + 1; j < jobLen; j++){
+      if(jobs[i][0] > jobs[j][0]){
+        int temp = jobs[i][0];
+        jobs[i][0] = jobs[j][0];
+        jobs[j][0] = temp;
+        
+        temp = jobs[i][1];
+        jobs[i][1] = jobs[j][1];
+        jobs[j][1] = temp;
+      }
+    }
+  }
+
+  while(!isEnd){
+    bool hasPush = false;
+    int minTime = 1001;
+
+    for(int i = jobNum; i < jobLen; i++){
+      if(jobs[i][0] <= diskTime){
+        jobQueue[qEnd][0] = jobs[i][0];
+        jobQueue[qEnd][1] = jobs[i][1];
+        hasPush = true;
+        jobNum = i + 1;
+        qEnd++;
+      } else {
+        minTime = minTime > jobs[i][0] ? minTime : jobs[i][0];
+        break;
+      }
+    }
+
+    if(hasPush){
+      for(int i = qStart; i < qEnd - 1; i++){
+        for(int j = i + 1; j < qEnd; j++){
+          if(jobQueue[i][1] > jobQueue[j][1]){
+            int temp = jobQueue[i][0];
+            jobQueue[i][0] = jobQueue[j][0];
+            jobQueue[j][0] = temp;
+            
+            temp = jobQueue[i][1];
+            jobQueue[i][1] = jobQueue[j][1];
+            jobQueue[j][1] = temp;
+          }
+        }
+      }
+    }
+
+    if(qStart == qEnd){
+      if(jobNum >= jobLen - 1){
+        isEnd = true;
+      } else {
+        diskTime = minTime;
+      }
+    } else {
+      diskTime += jobQueue[qStart][1]; 
+      answer += diskTime - jobQueue[qStart][0];
+      qStart++;
+    }
+  }
+
+  return answer / jobLen;
 }
